2908fb109c617127f1b6e0fc0f904515c0d1dd60
Takayuki Shimada <taka@tsmd.jp>
2019-07-12 13:41:03 +0900

    mklink.bat

---
+++ mklink.bat
@@ -0,0 +1,5 @@
+mklink %HOMEPATH%\.gvimrc       %~dp0\.gvimrc
+mklink %HOMEPATH%\.ideavimrc    %~dp0\.ideavimrc
+mklink %HOMEPATH%\.vimperatorrc %~dp0\.vimperatorrc
+mklink %HOMEPATH%\.vimrc        %~dp0\.vimrc
+mklink %HOMEPATH%\.vrapperrc    %~dp0\.vrapperrc
