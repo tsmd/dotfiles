3dbc1ba218811825bb1ecc9b94da3a6bc7fa7d58
Takayuki Shimada <taka@tsmd.jp>
2021-03-21 07:49:22 +0900

    Update mklink.bat

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,3 @@
 mklink %HOMEPATH%\.gvimrc       %~dp0\.gvimrc
 mklink %HOMEPATH%\.ideavimrc    %~dp0\.ideavimrc
-mklink %HOMEPATH%\.vimperatorrc %~dp0\.vimperatorrc
 mklink %HOMEPATH%\.vimrc        %~dp0\.vimrc
-mklink %HOMEPATH%\.vrapperrc    %~dp0\.vrapperrc
